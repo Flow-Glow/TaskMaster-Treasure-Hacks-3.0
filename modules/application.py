cade01b6231a7d5d346f0104a347e6e2fe5958ae
Vinh Tuan Huynh <thuynh2001.dw@gmail.com>
2023-01-14 16:32:36 +0700

    Create a responsive duration slider that ony appear when the user start adding tasks

--- modules/application.py
+++ modules/application.py
@@ -12,13 +12,20 @@ class App(ft.UserControl):
 
     def build(self):
 
+        # App Title
         self.title = ft.Text(value="TaskMaster", style="displayLarge",
                              height=100, color=ft.colors.CYAN, font_family="Consolas")
+        # A field for the user to input their task
         self.task_input_field = ft.TextField(
-            hint_text="Add your task", autofocus=True, width=500)
+            hint_text="Add your task and set time below", autofocus=True, width=500, on_change=self.on_text_field_changed)
+        # A button to add task 
         self.add_button = ft.FloatingActionButton(icon=ft.icons.ADD, shape=ft.CircleBorder(
         ), bgcolor=ft.colors.CYAN_300, on_click=self.on_add_button_clicked)
+        # A column 
         self.columns = ft.Column()
+        # Make a responsive view for time slider
+        self.time_slider = ft.Slider(min=0, max=60, divisions=60, label="Duration {value} minutes")
+        self.time_slider_view = ft.Column(controls=[ft.Text(value="Choose duration via slider: "), self.time_slider], visible=False)
 
         # This will return what the user see
         return ft.Column(
@@ -27,8 +34,9 @@ class App(ft.UserControl):
                 # First row for title
                 ft.Row([self.title], alignment="center"),
                 # Second for the inputting task
-                ft.Row([self.task_input_field, self.add_button],
-                       alignment="center"),
+                ft.Row([self.task_input_field, self.add_button], alignment="center"),
+                # Third row for task duration slider
+                ft.Row([self.time_slider_view], alignment="center"),
                 # Return self.column so later we can add tasks below this column
                 self.columns
             ])
@@ -45,4 +53,14 @@ class App(ft.UserControl):
             self.columns.controls.append(new_task)
             # Remove the input field
             self.task_input_field.value = ""
+            self.time_slider_view.visible = False
+            self.update()
+
+    # This function detect whenever the user enter anything to make the time slider view visible
+    def on_text_field_changed(self, e):
+        if self.task_input_field.value:
+            self.time_slider_view.visible = True
+            self.update()
+        else:
+            self.time_slider_view.visible = False
             self.update()
