86b2f8fc03ced8eac4582b82ea9e3f548e45beeb
Vinh Tuan Huynh <thuynh2001.dw@gmail.com>
2023-01-14 08:43:07 +0700

    fix modules import statement

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 # Path: Main.py
-import Modules
+from . import modules
 import flet
 from flet import Page
-
 
 def main(page: Page):
     page.title = "Task Master"
     page.horizontal_alignment = "center"
     page.update()
-
 
 flet.app(target=main)
